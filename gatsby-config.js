7d1a8491f57043d2f8d57ca9111f3442d310862a
oxmap <oxmap@yandex.com>
2019-09-23 20:44:41 +0700

    redesign Обновлены библиотеки. Добавлен yarn.lock файл.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-sass',
+    'gatsby-plugin-resolve-src',
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        includePaths: ["src/assets/scss"],
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
